ea45237f36de1aebeb5a5a06185c0b92ca5f4e94
Spy13x8 <136952143+Spy13x8@users.noreply.github.com>
2024-01-20 07:30:42 +0000

    added new code

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello():
+    return "hello"
+
+@app.route("/name")
+def name():
+    return "sahil"
